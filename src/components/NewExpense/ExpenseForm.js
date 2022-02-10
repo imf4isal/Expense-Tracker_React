84f8f2092773bc1f46c1029a104aaf7d03469ccc
Faisal <md.faisal1899@yahoo.com>
2022-02-10 23:32:56 +0600

    lifting state up. send data from expense form to app.js

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = props => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
@@ -29,8 +29,9 @@ const ExpenseForm = () => {
     };
 
     console.log(expenseData);
-
     console.log(expenseData);
+
+    props.onSaveExpenseData(expenseData);
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
